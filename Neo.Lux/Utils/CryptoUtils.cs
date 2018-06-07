a8facf59b6ec4192dd6e281a2fc14dbc6bb9fe67
Relfos <relfos@gmail.com>
2018-06-07 12:20:23 +0100

    Initial support for crypto via BouncyCastle

--- Neo.Lux/Utils/CryptoUtils.cs
+++ Neo.Lux/Utils/CryptoUtils.cs
@@ -5,6 +5,10 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using Neo.Lux.Cryptography;
+using Org.BouncyCastle.Asn1.Nist;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Security;
 
 namespace Neo.Lux.Utils
 {
@@ -33,8 +37,63 @@ namespace Neo.Lux.Utils
             return message.Sha256().Sha256();
         }
 
+        private static byte[] TranscodeSignatureToConcat(byte[] derSignature, int outputLength)
+        {
+            if (derSignature.Length < 8 || derSignature[0] != 48) throw new Exception("Invalid ECDSA signature format");
+
+            int offset;
+            if (derSignature[1] > 0)
+                offset = 2;
+            else if (derSignature[1] == 0x81)
+                offset = 3;
+            else
+                throw new Exception("Invalid ECDSA signature format");
+
+            var rLength = derSignature[offset + 1];
+
+            int i = rLength;
+            while (i > 0
+                   && derSignature[offset + 2 + rLength - i] == 0)
+                i--;
+
+            var sLength = derSignature[offset + 2 + rLength + 1];
+
+            int j = sLength;
+            while (j > 0
+                   && derSignature[offset + 2 + rLength + 2 + sLength - j] == 0)
+                j--;
+
+            var rawLen = Math.Max(i, j);
+            rawLen = Math.Max(rawLen, outputLength / 2);
+
+            if ((derSignature[offset - 1] & 0xff) != derSignature.Length - offset
+                || (derSignature[offset - 1] & 0xff) != 2 + rLength + 2 + sLength
+                || derSignature[offset] != 2
+                || derSignature[offset + 2 + rLength] != 2)
+                throw new Exception("Invalid ECDSA signature format");
+
+            var concatSignature = new byte[2 * rawLen];
+
+            Array.Copy(derSignature, offset + 2 + rLength - i, concatSignature, rawLen - i, i);
+            Array.Copy(derSignature, offset + 2 + rLength + 2 + sLength - j, concatSignature, 2 * rawLen - j, j);
+
+            return concatSignature;
+        }
+
         public static byte[] Sign(byte[] message, byte[] prikey, byte[] pubkey)
         {
+            var signer = SignerUtilities.GetSigner("SHA256withECDSA");
+            var curve = NistNamedCurves.GetByName("P-256");
+            var dom = new ECDomainParameters(curve.Curve, curve.G, curve.N, curve.H);
+            ECKeyParameters privateKeyParameters = new ECPrivateKeyParameters(new BigInteger(1, prikey), dom);
+
+            signer.Init(true, privateKeyParameters);
+            signer.BlockUpdate(message, 0, message.Length);
+            var sig = signer.GenerateSignature();
+
+            return TranscodeSignatureToConcat(sig, 64);
+
+#if OLD_VERSION
 #if NET461
             const int ECDSA_PRIVATE_P256_MAGIC = 0x32534345;
             prikey = BitConverter.GetBytes(ECDSA_PRIVATE_P256_MAGIC).Concat(BitConverter.GetBytes(32)).Concat(pubkey).Concat(prikey).ToArray();
@@ -57,6 +116,7 @@ namespace Neo.Lux.Utils
             {
                 return ecdsa.SignData(message, HashAlgorithmName.SHA256);
             }
+#endif
 #endif
         }
 
