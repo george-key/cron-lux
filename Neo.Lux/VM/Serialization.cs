d0afbd3d654639f2cf2250149a35219d6ff161a0
Sergio Flores <relfos@gmail.com>
2018-07-17 19:52:54 +0100

    Added support for Runtime.Serialize

---
+++ Neo.Lux/VM/Serialization.cs
@@ -0,0 +1,100 @@
+﻿using Neo.Lux.Utils;
+using Neo.Lux.VM.Types;
+using System;
+using System.IO;
+using System.Numerics;
+using VMArray = Neo.Lux.VM.Types.Array;
+using VMBoolean = Neo.Lux.VM.Types.Boolean;
+
+namespace Neo.Lux.VM
+{
+    internal enum StackItemType : byte
+    {
+        ByteArray = 0x00,
+        Boolean = 0x01,
+        Integer = 0x02,
+        InteropInterface = 0x40,
+        Array = 0x80,
+        Struct = 0x81,
+        Map = 0x82,
+    }
+
+    public static class Serialization
+    {
+        public static void SerializeStackItem(StackItem item, BinaryWriter writer)
+        {
+            switch (item)
+            {
+                case ByteArray _:
+                    writer.Write((byte)StackItemType.ByteArray);
+                    writer.WriteVarBytes(item.GetByteArray());
+                    break;
+                case VMBoolean _:
+                    writer.Write((byte)StackItemType.Boolean);
+                    writer.Write(item.GetBoolean());
+                    break;
+                case Integer _:
+                    writer.Write((byte)StackItemType.Integer);
+                    writer.WriteVarBytes(item.GetByteArray());
+                    break;
+                case InteropInterface _:
+                    throw new NotSupportedException();
+                case VMArray array:
+                    if (array is Struct)
+                        writer.Write((byte)StackItemType.Struct);
+                    else
+                        writer.Write((byte)StackItemType.Array);
+                    writer.WriteVarInt(array.Count);
+                    foreach (StackItem subitem in array)
+                        SerializeStackItem(subitem, writer);
+                    break;
+                case Map map:
+                    writer.Write((byte)StackItemType.Map);
+                    writer.WriteVarInt(map.Count);
+                    foreach (var pair in map)
+                    {
+                        SerializeStackItem(pair.Key, writer);
+                        SerializeStackItem(pair.Value, writer);
+                    }
+                    break;
+            }
+        }
+
+        public static StackItem DeserializeStackItem(BinaryReader reader)
+        {
+            StackItemType type = (StackItemType)reader.ReadByte();
+            switch (type)
+            {
+                case StackItemType.ByteArray:
+                    return new ByteArray(reader.ReadVarBytes());
+                case StackItemType.Boolean:
+                    return new VMBoolean(reader.ReadBoolean());
+                case StackItemType.Integer:
+                    return new Integer(new BigInteger(reader.ReadVarBytes()));
+                case StackItemType.Array:
+                case StackItemType.Struct:
+                    {
+                        VMArray array = type == StackItemType.Struct ? new Struct() : new VMArray();
+                        ulong count = reader.ReadVarInt();
+                        while (count-- > 0)
+                            array.Add(DeserializeStackItem(reader));
+                        return array;
+                    }
+                case StackItemType.Map:
+                    {
+                        Map map = new Map();
+                        ulong count = reader.ReadVarInt();
+                        while (count-- > 0)
+                        {
+                            StackItem key = DeserializeStackItem(reader);
+                            StackItem value = DeserializeStackItem(reader);
+                            map[key] = value;
+                        }
+                        return map;
+                    }
+                default:
+                    throw new FormatException();
+            }
+        }
+    }
+}
